b16831ebbb6a2162d051b89975bcc4c8742f170a
Rozilio <hagayroz@gmail.com>
2017-07-09 21:38:20 +0300

    start project

---
+++ program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hi agian");
+        }
+    }
+}
